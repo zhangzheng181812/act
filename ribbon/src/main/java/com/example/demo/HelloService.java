ac63ebdfaa4ec6e97d838cb1b7309e5e8e700e26
zhang <568125122@qq.com>
2017-11-23 17:01:57 +0800

    0999

--- ribbon/src/main/java/com/example/demo/HelloService.java
+++ ribbon/src/main/java/com/example/demo/HelloService.java
@@ -14,7 +14,7 @@ public class HelloService {
     RestTemplate restTemplate;
 
     public String hiService(String name) {
-        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+        return restTemplate.getForObject("http://SERVICE-HI/hi?1111111111111111="+name,String.class);
     }
 
 }
